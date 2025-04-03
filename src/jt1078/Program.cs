2f584e3f63388e733ca285bc1b4930c751721c76
hedawei <hhqhqs@126.com>
2025-04-03 08:19:10 +0800

    新加项

---
+++ src/jt1078/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Hosting;
+using SuperSocket.Server.Host;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace jt1078
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = SuperSocketHostBuilder.Create<MyPackage, MyPackageFilter>()
+                .UsePackageHandler(async (s, p) =>
+                {
+                    // handle package
+                   
+
+                    Console.WriteLine(s.Server.SessionCount);
+                    await Task.Delay(0);
+                })
+                 .ConfigureSuperSocket(options =>
+                 {
+                     //options.Name = "CustomProtocol Server";
+                     //options.Listeners = new List<ListenOptions>
+                     //{
+                     //    new ListenOptions
+                     //    {
+                     //        Ip = "Any",
+                     //        Port = 4040
+                     //    }
+                     //};
+                 })
+                 .ConfigureAppConfiguration((hostCtx, configBuilder) =>
+                 {
+                     configBuilder.AddJsonFile("appsettings.json"); // 加载 JSON 配置文件‌
+                 })
+                .ConfigureLogging((hostCtx, loggingBuilder) =>
+                {
+                    loggingBuilder.AddConsole();
+                })
+                .Build();
+
+            await host.RunAsync();
+        }
+    }
+}
